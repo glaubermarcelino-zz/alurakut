4cecbd0e7b39ab7f78d5899ed804a0addc4c2d32
GTS Soluções Tecnológicas <gmsoft.informatica@gmail.com>
2021-07-16 16:34:36 -0300

    Adicionado validações antes de logar

--- pages/login.js
+++ pages/login.js
@@ -2,33 +2,58 @@ import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
 import nookies from 'nookies';
+import { ToastContainer, toast } from 'react-toastify';
 
 export default function LoginScreen() {
   const [userGithub, setUserGitHub] = useState('');
+  const [usuarioExiste, setUsuarioExiste] = useState(false);
   const router = useRouter();
 
+
+  const handleUsuarioGithubExiste = (usuario) => {
+    axios.get(`${process.env.NEXT_PUBLIC_GITHUB_API}/users/${usuario}`)
+    .then((response) =>{
+        if(response.status == 200){
+          setUsuarioExiste(true);
+        }
+    }).catch(erro => setUsuarioExiste(false))
+   return usuarioExiste;
+  }
   const handleLoginGitHub = (event) => {
     event.preventDefault();
-    const user = JSON.stringify({githubUser:userGithub});
+    if (handleUsuarioGithubExiste(userGithub)) {
+      const user = JSON.stringify({ githubUser: userGithub });
 
-    axios.post('https://alurakut.vercel.app/api/login',user)
-    .then(respostaDoServer =>{
-      const dadosDaResposta =  respostaDoServer;
-      const token = dadosDaResposta.data.token;
-      nookies.set(null, 'USER_TOKEN', token, {
-          path: '/',
-          maxAge: 86400 * 7 
-      })
-      nookies.set(null, 'USER', userGithub, {
-        path: '/',
-        maxAge: 86400 * 7 
-    })
-      router.push('/')
-    }).catch(erro => console.log(erro)
-    )
+      axios.post('https://alurakut.vercel.app/api/login', user)
+        .then(respostaDoServer => {
+          const dadosDaResposta = respostaDoServer;
+          const token = dadosDaResposta.data.token;
+          nookies.set(null, 'USER_TOKEN', token, {
+            path: '/',
+            maxAge: 86400 * 7
+          })
+          nookies.set(null, 'USER', userGithub, {
+            path: '/',
+            maxAge: 86400 * 7
+          })
+          router.push('/')
+        }).catch(erro => console.log(erro)
+        )
+    } else {
+      toast.error(`Usuário inválido!`);
+    }
   }
   return (
     <main style={{ display: 'flex', flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+       <ToastContainer
+          position="top-right"
+          autoClose="5000"
+          hideProgressBar={false}
+          closeOnClick={true}
+          pauseOnHover={true}
+          draggable={true}
+          progress={undefined}
+        />
       <div className="loginScreen">
         <section className="logoArea">
           <img src="https://alurakut.vercel.app/logo.svg" />
